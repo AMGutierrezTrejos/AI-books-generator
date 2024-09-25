033350c6c5f1472733564899c9468106f6a39ff8
MauroTre <m.gutierrez7@hotmail.com>
2024-09-24 23:53:41 -0500

    Creating Navbar

--- ai-web-story/app/_components/Header.tsx
+++ ai-web-story/app/_components/Header.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import Image from "next/image";
 
 function Header() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Navbar>
+      <NavbarContent>
+        <NavbarBrand>
+          <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
+          <h2 className="font-bold tewxt-2xl text-primary ml-3">AI Web Story</h2>
+        </NavbarBrand>
+      </NavbarContent>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
--- ai-web-story/app/page.tsx
+++ ai-web-story/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@nextui-org/button";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
       {/* Header */}
-      
+      <Header />
       {/* Hero */}
     </div>
   );
